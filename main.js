18993b3a3ca980be83be7a44d595dcfeccc07619
eloymoraleswork <103529481+eloymoraleswork@users.noreply.github.com>
2022-09-25 22:14:23 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+let ventas = [];
+
+function cargarPrecio() {
+    let precio = parseInt(prompt("Ingrese el precio de la venta"));
+
+    if (precio && precio > 0) {
+        ventas.push(precio);
+        alert("Su venta fue de " + precio + " pesos");
+    } else {
+        alert("Debes escribir un precio mayor a 0");
+    }
+}
+
+function calcularTotal() {
+    let sumaTotal = 0;
+
+    for (var i = 0; i < ventas.length; i++) {
+        console.log(ventas);
+        sumaTotal += ventas[i];
+    }
+
+    alert("Total de vendido: " + sumaTotal + " pesos");
+}
